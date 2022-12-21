0fc3e2056be2ed5ceba9edf35ced6d8afcb3e31c
Abhi <abhisheksonu174@gmail.com>
2022-12-21 15:06:31 +0530

    rkudfy

---
+++ src/test/java/com/crm/organization/test14.java
@@ -0,0 +1,25 @@
+package com.crm.organization;
+
+import org.testng.annotations.Test;
+
+public class test14 {
+
+	@Test
+
+	public void test1()
+	{
+		System.out.println("test1");
+	}
+	@Test
+
+	public void test2()
+	{
+		System.out.println("test2");
+	}
+	@Test
+
+	public void test3()
+	{
+		System.out.println("test3");
+	}
+}
